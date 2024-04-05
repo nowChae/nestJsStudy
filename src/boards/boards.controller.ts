82238802501384fb1952644be58400473ad40dec
nowChae <nowchae23@gmail.com>
2024-04-06 02:03:22 +0900

    게시판 crud

---
+++ src/boards/boards.controller.ts
@@ -0,0 +1,39 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { BoardsService } from './boards.service';
+import { Board, BoardStatus } from './board.model';
+import { CreateBoardDto } from './dto/create-board.dto';
+
+@Controller('boards') // localhost:3000/boards
+export class BoardsController {
+    constructor(private boardService : BoardsService){}
+     
+    @Get('/') // localhost:3000/boards/ <-- 
+    getAllBoard() :Board[]{ // 모든 게시글을 가져옴
+        return this.boardService.getAllBoards();
+    }
+
+    @Post()
+    createBoard(
+        @Body() createBoardDto: CreateBoardDto 
+    ):Board{
+        return this.boardService.createBoard(createBoardDto);
+    }
+
+    @Get('/:id')
+    getBoardById(@Param('id') id : string):Board{
+        return this.boardService.getBoardById(id);
+    }
+
+    @Delete('/:id')
+    deleteBoard(@Param('id') id : string): void{
+        this.boardService.deleteBoard(id); 
+    }
+
+    @Patch('/:id/status')
+    updateBoardStatus(
+        @Param('id') id : string,
+        @Body('status') status : BoardStatus 
+    ):Board{ 
+        return this.boardService.updateBoardStatus(id, status); 
+    }
+}
