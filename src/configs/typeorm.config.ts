74de48fe37d7242cea7ade2dca78a41fff1419e6
nowChae <nowchae23@gmail.com>
2024-04-07 21:58:46 +0900

    db 연결

---
+++ src/configs/typeorm.config.ts
@@ -0,0 +1,13 @@
+import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+import { dirname } from "path";
+
+ export const typeORMConfig : TypeOrmModuleOptions = {
+    type : 'postgres',
+    host : 'localhost',
+    port : 5432,
+    username : 'postgres',
+    password : 'postgres',
+    database : 'board-app',
+    entities : [__dirname+ '/../**/*.entity.{js,ts}'],
+    synchronize : true
+ }
