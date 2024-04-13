27fbcc1ff8af59ec540f5d7455226f3f6c74833e
nowChae <nowchae23@gmail.com>
2024-04-14 00:14:28 +0900

    jwt 토큰 생성 - 로그인

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,21 +2,29 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserRepository } from './user.repository';
 import { AuthCredentialDto } from './dto/auth-credential.dto';
 import * as bcrypt from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-    constructor( private userRepository:UserRepository){}
+    constructor( 
+        private userRepository:UserRepository,
+        private jwtService : JwtService
+    ){}
 
     async signUp(authCredentialDto : AuthCredentialDto) : Promise<void>{
         return this.userRepository.createUser(authCredentialDto);
     }
 
-    async signIn(authCredentialDto : AuthCredentialDto):Promise<string>{
+    async signIn(authCredentialDto : AuthCredentialDto):Promise<{accessToken:string}>{
         const {username, password} = authCredentialDto;
         const user = await this.userRepository.findOne({where : {username}});
 
         if (user && (await bcrypt.compare(password, user.password))){
-            return 'login success';
+            //유저 토큰 생성 ( Secret + Payload )
+            const payload = {username};
+            const accessToken = await this.jwtService.sign(payload); 
+
+            return {accessToken};
         }else{
             throw new UnauthorizedException('login failed');
         }
